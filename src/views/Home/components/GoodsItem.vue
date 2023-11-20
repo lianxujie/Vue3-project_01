72033f59cf7bf8d5d15535dff5c094757c9ba238
lxj <2256996378@qq.com>
2023-11-20 10:23:29 +0800

    Home静态结构编写

---
+++ src/views/Home/components/GoodsItem.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+defineProps({
+    goods: {
+        type: Object,
+        default:()=>{}
+    }
+})
+
+</script>
+
+
+<template>
+<RouterLink  to="/" class="goods-item">
+   <img v-img-lazy="goods.picture" alt=""/>
+   <p class="name ellipsis">{{ goods.name }}</p>
+    <p class="desc ellipsis">{{ goods.desc }}</p>
+    <p class="price">&yen;{{ goods.price }}</p>
+</RouterLink>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
